79fc60f35a6d7840f84722556117e8ba1b61b233
Eddie Yang <43977631+EddieYang211@users.noreply.github.com>
2025-05-28 13:44:27 -0500

    1

--- src/keyATM_cov.cpp
+++ src/keyATM_cov.cpp
@@ -243,9 +243,29 @@ double keyATMcov::loglik_total()
   
   for (int k = 0; k < num_topics; ++k) {
     for (int v = 0; v < num_vocab; ++v) {
-      loglik += mylgamma(beta + n_kv(k, v)) - mylgamma(beta);
+      loglik += mylgamma(beta + n_s0_kv(k, v)) - mylgamma(beta);
     }
-    loglik += mylgamma(beta * (double)num_vocab) - mylgamma(beta * (double)num_vocab + n_k(k));
+    
+    // word normalization
+    loglik += mylgamma(beta * (double)num_vocab) - mylgamma(beta * (double)num_vocab + n_s0_k(k));
+
+    if (k < keyword_k) {
+      // For keyword topics
+      
+      // n_s1_kv
+      for (SparseMatrix<double,RowMajor>::InnerIterator it(n_s1_kv, k); it; ++it) {
+        loglik += mylgamma(beta_s + it.value()) - mylgamma(beta_s);
+      }
+      loglik += mylgamma(beta_s * (double)keywords_num[k]) - mylgamma(beta_s * (double)keywords_num[k] + n_s1_k(k));
+
+      // Normalization
+      loglik += mylgamma(prior_gamma(k, 0) + prior_gamma(k, 1)) - mylgamma(prior_gamma(k, 0)) - mylgamma(prior_gamma(k, 1));
+
+      // s
+      loglik += mylgamma(n_s0_k(k) + prior_gamma(k, 1))
+                - mylgamma(n_s1_k(k) + prior_gamma(k, 0) + n_s0_k(k) + prior_gamma(k, 1))
+                + mylgamma(n_s1_k(k) + prior_gamma(k, 0));
+    }
   }
 
   loglik += alpha_loglik();
